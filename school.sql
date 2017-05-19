c5d915e4c001c6fc603f2a9540a818374763d63f
Ivan Makhambetov <i.makhambetov@gmail.com>
2017-05-19 14:45:33 +0600

    data base backup updated

--- school.sql
+++ school.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Хост: 127.0.0.1
--- Время создания: Май 18 2017 г., 14:19
+-- Время создания: Май 19 2017 г., 10:44
 -- Версия сервера: 10.1.9-MariaDB
 -- Версия PHP: 5.6.15
 
@@ -27,7 +27,8 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `classes` (
-  `id` varchar(3) NOT NULL,
+  `id` int(3) UNSIGNED NOT NULL,
+  `name` varchar(3) NOT NULL,
   `teacher_name` varchar(65) NOT NULL,
   `av_mark` float UNSIGNED DEFAULT '0',
   `students_count` int(3) UNSIGNED NOT NULL DEFAULT '0'
@@ -37,9 +38,10 @@ CREATE TABLE `classes` (
 -- Дамп данных таблицы `classes`
 --
 
-INSERT INTO `classes` (`id`, `teacher_name`, `av_mark`, `students_count`) VALUES
-('10Б', 'Дональд Дак', 4, 3),
-('11А', 'Mickey Mouse', 2.75, 3);
+INSERT INTO `classes` (`id`, `name`, `teacher_name`, `av_mark`, `students_count`) VALUES
+(0, '11А', 'James Bond', 3.5, 4),
+(1, '10Б', 'Дональд Дак', 4, 3),
+(2, '11Г', 'Mickey Mouse', 0, 0);
 
 -- --------------------------------------------------------
 
@@ -63,10 +65,10 @@ CREATE TABLE `students` (
 INSERT INTO `students` (`id`, `student_name`, `class_id`, `mark`, `start_date`, `end_date`) VALUES
 (1, 'Маша', '10Б', 3, '2017-05-01', '0000-00-00'),
 (2, 'Саша', '10Б', 5, '0000-00-00', '2017-05-05'),
-(3, 'Паша', '11А', 4, '2017-04-02', NULL),
-(4, 'Вася', '11А', 3, '2017-04-10', NULL),
-(25, 'Катя', '11А', 2, '2015-11-30', '2016-12-31'),
-(26, 'Коля', '11А', 2, '2017-05-05', '2017-05-12');
+(3, 'Паша', '11А', 4, '2017-04-03', '0000-00-00'),
+(4, 'Вася', '11А', 3, '2017-04-10', '0000-00-00'),
+(25, 'Катя', '11А', 2, '2017-05-12', '0000-00-00'),
+(26, 'Коля', '11А', 5, '0000-00-00', '2017-05-10');
 
 --
 -- Индексы сохранённых таблиц
@@ -89,10 +91,15 @@ ALTER TABLE `students`
 --
 
 --
+-- AUTO_INCREMENT для таблицы `classes`
+--
+ALTER TABLE `classes`
+  MODIFY `id` int(3) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+--
 -- AUTO_INCREMENT для таблицы `students`
 --
 ALTER TABLE `students`
-  MODIFY `id` int(3) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=27;
+  MODIFY `id` int(3) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=29;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
